2817bf1e7cb3b235df18680ba5afab34988f7733
Dmitrijus Byckovas <boyuda96@gmail.com>
2021-11-27 01:19:09 +0200

    Add project files.

---
+++ Task 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var total = 0;
+
+            Console.WriteLine("Please enter number of elements in a numeric array: ");
+            var input = int.Parse(Console.ReadLine());
+
+            var numericArray = new int[input];
+
+            //Taking input then converting and adding to an array
+            Console.WriteLine($"Please enter {numericArray.Length} elements one by one: ");
+            for (int i = 0; i < input; i++)
+            {
+                var elemenntOfArray = Convert.ToInt32(Console.ReadLine());
+                numericArray[i] = elemenntOfArray;
+            }
+
+            Console.WriteLine("Array with elements: [{0}]", string.Join(",", numericArray));
+
+            //Finding lowest and Largest numbers positions
+            var lowestNumber = Array.IndexOf(numericArray, numericArray.Min());
+            var largestNumber = Array.LastIndexOf(numericArray, numericArray.Max());
+
+            //Adding conditional statements to count the sum of the elements
+            if (lowestNumber < largestNumber)
+            {
+                for (int i = lowestNumber; i <= largestNumber; i++)
+                {
+                    total += numericArray[i];
+                }
+            } 
+            else if (lowestNumber>largestNumber)
+            {
+                for (int i = lowestNumber; i >= largestNumber; i--)
+                {
+                    total += numericArray[i];
+                }
+            } 
+            else if (numericArray.Length ==2)
+            {
+                total = numericArray[lowestNumber] + numericArray[largestNumber];
+            }
+
+            Console.WriteLine("The sum of the required elements = " +total);  
+        }
+    }
+}
